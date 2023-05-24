f8ea2e74fa56a76c4be65bbe92671ab7e314da9a
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-05-24 05:00:29 -0300

    DIO - Blog Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { MenutitleComponent } from './components/menutitle/menutitle.component';
+import { BigcardComponent } from './components/bigcard/bigcard.component';
+import { SmallcardComponent } from './components/smallcard/smallcard.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent,
+    MenutitleComponent,
+    BigcardComponent,
+    SmallcardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/menutitle/menutitle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menutitle',
+  templateUrl: './menutitle.component.html',
+  styleUrls: ['./menutitle.component.css']
+})
+export class MenutitleComponent {
+
+}
